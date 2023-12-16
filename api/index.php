c0e5c1af63b1c95b3870c89ea45976bc5734afd7
CodeAWProject <activityware@gmail.com>
2023-12-17 00:29:47 +0100

    Adding a controller class to decide the response

--- api/index.php
+++ api/index.php
@@ -1,7 +1,6 @@
-<?php 
+<?php
 
-$path =  parse_url($_SERVER["REQUEST_URI"], PHP_URL_PATH);
-
+$path = parse_url($_SERVER["REQUEST_URI"], PHP_URL_PATH);
 
 $parts = explode("/", $path);
 
@@ -9,13 +8,24 @@ $resource = $parts[4];
 
 $id = $parts[5] ?? null;
 
-echo $resource, ", ", $id;
-
-echo $_SERVER["REQUEST_METHOD"];
-
-if($resource != "tasks") {
-
-    // header("{$_SERVER['SERVER_PROTOCOL']} 404 Not Found");
+if ($resource != "tasks") {
+    
+    //header("{$_SERVER['SERVER_PROTOCOL']} 404 Not Found");
     http_response_code(404);
     exit;
 }
+
+require dirname(__DIR__) . "/src/TaskController.php";
+
+$controller = new TaskController;
+
+$controller->processRequest($_SERVER['REQUEST_METHOD'], $id);
+
+
+
+
+
+
+
+
+
